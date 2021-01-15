5bea3b6c1d349c57373c0c1b163e9f90aefaa740
Husan753 <shrv10@bk.ru>
2021-01-15 17:01:08 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public abstract class Hero implements HavingSuperAbility {
+
+    private int health,damage;
+    private String attackType;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getAttackType() {
+        return attackType;
+    }
+
+    public void setAttackType(String attackType) {
+        this.attackType = attackType;
+    }
+}
+
+
+
